f8cc478511353743567a008c164fd6ff18092882
edgasanc <edgasanc@gmail.com>
2015-09-03 18:28:22 -0500

    Componente Grafico
    Archivos gráficos

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,19 +6,19 @@ $this->title = 'EDGASANC-CRM';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
+    <!--<div class="jumbotron">
         <h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    </div>-->
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Clientes</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -28,7 +28,7 @@ $this->title = 'EDGASANC-CRM';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Pedidos</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -38,7 +38,7 @@ $this->title = 'EDGASANC-CRM';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Almacen</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -46,6 +46,66 @@ $this->title = 'EDGASANC-CRM';
                     fugiat nulla pariatur.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+            <div class="col-lg-6">
+                <?php
+                      use miloschuman\highcharts\Highcharts;
+
+                      echo Highcharts::widget([
+                      'options' => [
+                          'title' => ['text' => 'Cantidad Ventas Ultima Semana'],
+                          'xAxis' => [
+                              'categories' => ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado']
+                          ],
+                          'yAxis' => [
+                          'title' => ['text' => 'Ventas Realizadas']
+                          ],
+                          'series' => [
+                              ['name' => 'Jane', 'data' => [1, 0, 4, 5, 6, 7]],
+                              ['name' => 'John', 'data' => [5, 7, 3, 4, 2, 0]],
+                              ['name' => 'Joe', 'data' => [3, 1, 6, 2, 0, 4]]
+                          ]
+                      ]
+                      ]);
+                ?>
+            </div>
+            <div class="col-lg-6">
+                <?php
+                //use miloschuman\highcharts\Highcharts;
+                use yii\web\JsExpression;
+
+                echo Highcharts::widget([
+                    'scripts' => [
+                        'modules/exporting',
+                        'themes/grid-light',
+                    ],
+                    'options' => [
+                        'title' => [
+                            'text' => 'Monto de Ventas Ultima Semana',
+                        ],
+                        'xAxis' => [
+                            'categories' => ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'],
+                        ],
+                        'series' => [
+                            [
+                                'type' => 'column',
+                                'name' => 'Jane',
+                                'data' => [50000, 80000, 100000, 60000, 40000],
+                            ],
+                            [
+                                'type' => 'column',
+                                'name' => 'John',
+                                'data' => [20000, 30000, 50000, 70000, 60000],
+                            ],
+                            [
+                                'type' => 'column',
+                                'name' => 'Joe',
+                                'data' => [40000, 30000, 30000, 90000, 10000],
+                            ],
+                        ],
+                    ]
+                ]);
+                ?>
             </div>
         </div>
 
