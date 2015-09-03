fc78a8fd620b2062e22f7abf15eb56bbee5243b7
edgasanc <edgasanc@gmail.com>
2015-09-03 17:38:37 -0500

    DB
    Actualización Base de Datos

--- crm.sql
+++ crm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-09-2015 a las 18:29:09
--- Versión del servidor: 5.6.26
--- Versión de PHP: 5.6.12
+-- Tiempo de generación: 04-09-2015 a las 00:37:37
+-- Versión del servidor: 5.6.25
+-- Versión de PHP: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,26 +23,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `carropedidos`
+--
+
+CREATE TABLE IF NOT EXISTS `carropedidos` (
+  `idCarroPedidos` int(11) NOT NULL,
+  `pedidos_idPedidos` int(11) NOT NULL,
+  `productos_idProducos` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `estadoCarroPedidos` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `clientes`
 --
 
 CREATE TABLE IF NOT EXISTS `clientes` (
-  `idCliente` int(10) NOT NULL COMMENT 'ID del cliente',
-  `razonSocial` varchar(200) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ingrese la razón social del cliente o nombre del cliente',
-  `direccion` varchar(200) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ingrese la dirección del cliente',
-  `barrio` varchar(100) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ingrese el barrio de domicilio del cliente',
-  `telefono` varchar(12) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ingrese el numero telefonico del cliente',
-  `nit` varchar(50) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ingrese el NIT o CEDULA del cliente',
-  `fechaCreacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `idClientes` int(11) NOT NULL,
+  `razonSocial` varchar(200) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `barrio` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `telefono` int(10) DEFAULT NULL,
+  `nit` int(10) DEFAULT NULL,
+  `nitVer` int(1) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`idCliente`, `razonSocial`, `direccion`, `barrio`, `telefono`, `nit`, `fechaCreacion`) VALUES
-(1, 'Integra SAS', 'Carrera 24 No 45 36', 'Galerias', '2202790', '8345678-1', '2015-09-01 23:55:51'),
-(2, 'Escuela Superior de Administración Pública', 'Calle 44 No 53 37 CAN', 'CAN', '2202790', '90034592-2', '2015-09-02 00:13:59');
+INSERT INTO `clientes` (`idClientes`, `razonSocial`, `barrio`, `telefono`, `nit`, `nitVer`, `create_time`, `update_time`) VALUES
+(1, 'Escuela Superior de Administración Pública', 'CAN', 2202790, 899999054, 7, '2015-09-03 22:10:44', '2015-09-03 22:12:06');
 
 -- --------------------------------------------------------
 
@@ -51,17 +67,33 @@ INSERT INTO `clientes` (`idCliente`, `razonSocial`, `direccion`, `barrio`, `tele
 --
 
 CREATE TABLE IF NOT EXISTS `embalaje` (
-  `idEmbalaje` int(10) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `idEmbalaje` int(11) NOT NULL,
+  `nombre` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `embalaje`
 --
 
-INSERT INTO `embalaje` (`idEmbalaje`, `nombre`) VALUES
-(1, 'Unidad'),
-(2, 'Caja');
+INSERT INTO `embalaje` (`idEmbalaje`, `nombre`, `create_time`, `update_time`) VALUES
+(1, 'Unidad', '2015-09-03 22:14:21', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+CREATE TABLE IF NOT EXISTS `entradas` (
+  `idEntradas` int(11) NOT NULL,
+  `productos_idProducos` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio` decimal(12,2) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -70,18 +102,47 @@ INSERT INTO `embalaje` (`idEmbalaje`, `nombre`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `impuestos` (
-  `id` int(10) NOT NULL,
-  `nombre` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `valor` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `idImpuesto` int(11) NOT NULL,
+  `nombre` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `valor` decimal(4,2) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `impuestos`
 --
 
-INSERT INTO `impuestos` (`id`, `nombre`, `valor`) VALUES
-(1, 'IVA General', 16),
-(2, 'IVA Especial', 5);
+INSERT INTO `impuestos` (`idImpuesto`, `nombre`, `valor`, `create_time`, `update_time`) VALUES
+(1, 'IVA General', '16.00', '2015-09-03 22:14:32', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE IF NOT EXISTS `inventario` (
+  `idAlmacen` int(11) NOT NULL,
+  `productos_idProducos` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE IF NOT EXISTS `pedidos` (
+  `idPedidos` int(11) NOT NULL,
+  `clientes_idClientes` int(11) NOT NULL,
+  `estadoPedido` int(1) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -90,81 +151,198 @@ INSERT INTO `impuestos` (`id`, `nombre`, `valor`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `productos` (
-  `idProductos` int(10) NOT NULL,
-  `codigo` int(10) NOT NULL,
-  `producto` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
-  `embalaje` int(1) NOT NULL,
-  `impuesto` int(2) NOT NULL,
-  `precio` int(11) NOT NULL,
-  `fechaCreacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `idProducos` int(11) NOT NULL,
+  `codigo` int(11) DEFAULT NULL,
+  `producto` varchar(200) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `precio` decimal(12,2) DEFAULT NULL,
+  `embalaje_idEmbalaje` int(11) NOT NULL,
+  `impuestos_idImpuesto` int(11) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`idProductos`, `codigo`, `producto`, `embalaje`, `impuesto`, `precio`, `fechaCreacion`) VALUES
-(1, 10001, 'ACT II Natural', 1, 16, 1690, '2015-09-02 00:28:34');
+INSERT INTO `productos` (`idProducos`, `codigo`, `producto`, `precio`, `embalaje_idEmbalaje`, `impuestos_idImpuesto`, `create_time`, `update_time`) VALUES
+(1, 10001, 'ACT II Natural', '1690.00', 1, 1, '2015-09-03 22:30:32', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salidas`
+--
+
+CREATE TABLE IF NOT EXISTS `salidas` (
+  `idSalidas` int(11) NOT NULL,
+  `pedidos_idPedidos` int(11) NOT NULL,
+  `productos_idProducos` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio` decimal(12,2) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `carropedidos`
+--
+ALTER TABLE `carropedidos`
+  ADD PRIMARY KEY (`idCarroPedidos`,`pedidos_idPedidos`,`productos_idProducos`),
+  ADD KEY `fk_carroPedidos_productos1_idx` (`productos_idProducos`),
+  ADD KEY `fk_carroPedidos_pedidos1_idx` (`pedidos_idPedidos`);
+
+--
 -- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`idCliente`),
-  ADD UNIQUE KEY `idCliente_2` (`idCliente`),
-  ADD KEY `idCliente` (`idCliente`);
+  ADD PRIMARY KEY (`idClientes`);
 
 --
 -- Indices de la tabla `embalaje`
 --
 ALTER TABLE `embalaje`
-  ADD PRIMARY KEY (`idEmbalaje`),
-  ADD UNIQUE KEY `idEmbalaje_2` (`idEmbalaje`),
-  ADD KEY `idEmbalaje` (`idEmbalaje`),
-  ADD KEY `idEmbalaje_3` (`idEmbalaje`);
+  ADD PRIMARY KEY (`idEmbalaje`);
+
+--
+-- Indices de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD PRIMARY KEY (`idEntradas`,`productos_idProducos`),
+  ADD KEY `fk_entradas_productos1_idx` (`productos_idProducos`);
 
 --
 -- Indices de la tabla `impuestos`
 --
 ALTER TABLE `impuestos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_2` (`id`),
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`idImpuesto`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`idAlmacen`,`productos_idProducos`),
+  ADD KEY `fk_almacen_productos1_idx` (`productos_idProducos`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`idPedidos`,`clientes_idClientes`),
+  ADD KEY `fk_pedidos_clientes1_idx` (`clientes_idClientes`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD KEY `idProductos` (`idProductos`);
+  ADD PRIMARY KEY (`idProducos`,`embalaje_idEmbalaje`,`impuestos_idImpuesto`),
+  ADD KEY `fk_productos_embalaje_idx` (`embalaje_idEmbalaje`),
+  ADD KEY `fk_productos_impuestos1_idx` (`impuestos_idImpuesto`);
+
+--
+-- Indices de la tabla `salidas`
+--
+ALTER TABLE `salidas`
+  ADD PRIMARY KEY (`idSalidas`,`pedidos_idPedidos`,`productos_idProducos`),
+  ADD KEY `fk_salidas_productos1_idx` (`productos_idProducos`),
+  ADD KEY `fk_salidas_pedidos1_idx` (`pedidos_idPedidos`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `carropedidos`
+--
+ALTER TABLE `carropedidos`
+  MODIFY `idCarroPedidos` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idCliente` int(10) NOT NULL AUTO_INCREMENT COMMENT 'ID del cliente',AUTO_INCREMENT=3;
+  MODIFY `idClientes` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `embalaje`
 --
 ALTER TABLE `embalaje`
-  MODIFY `idEmbalaje` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `idEmbalaje` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  MODIFY `idEntradas` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `impuestos`
 --
 ALTER TABLE `impuestos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `idImpuesto` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `idAlmacen` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `idPedidos` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `idProductos` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `idProducos` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `salidas`
+--
+ALTER TABLE `salidas`
+  MODIFY `idSalidas` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carropedidos`
+--
+ALTER TABLE `carropedidos`
+  ADD CONSTRAINT `fk_carroPedidos_pedidos1` FOREIGN KEY (`pedidos_idPedidos`) REFERENCES `pedidos` (`idPedidos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_carroPedidos_productos1` FOREIGN KEY (`productos_idProducos`) REFERENCES `productos` (`idProducos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD CONSTRAINT `fk_entradas_productos1` FOREIGN KEY (`productos_idProducos`) REFERENCES `productos` (`idProducos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `fk_almacen_productos1` FOREIGN KEY (`productos_idProducos`) REFERENCES `productos` (`idProducos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedidos_clientes1` FOREIGN KEY (`clientes_idClientes`) REFERENCES `clientes` (`idClientes`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_productos_embalaje` FOREIGN KEY (`embalaje_idEmbalaje`) REFERENCES `embalaje` (`idEmbalaje`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_productos_impuestos1` FOREIGN KEY (`impuestos_idImpuesto`) REFERENCES `impuestos` (`idImpuesto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `salidas`
+--
+ALTER TABLE `salidas`
+  ADD CONSTRAINT `fk_salidas_pedidos1` FOREIGN KEY (`pedidos_idPedidos`) REFERENCES `pedidos` (`idPedidos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_salidas_productos1` FOREIGN KEY (`productos_idProducos`) REFERENCES `productos` (`idProducos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
