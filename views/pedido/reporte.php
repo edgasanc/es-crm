15db9c8a3a50bb9b8ddf3c345528fe7dd3177542
root <jcesarrc@gmail.com>
2015-09-11 16:20:01 -0500

    OK

---
+++ views/pedido/reporte.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\helpers\Url;
+use kartik\datecontrol\DateControl;
+use app\models\Cliente;
+use app\models\Estado;
+use yii\helpers\ArrayHelper;
+use kartik\widgets\Select2;
+
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PedidoSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+$this->title = "Despacho";
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Pedidos'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+<div class="pedido-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="pedido-form">
+
+        <form id="frmFechaEntrega" method="POST" action="<?=Url::to(['/pedido/despacho-productos'])?>">
+            <label>Seleccione una fecha para consulta</label><br>
+            <input name="fecha" type="date" required/>
+            <button type="submit" class="btn btn-primary">Consultar</button>
+            
+        </form>
+    
+    </div>
+    
+
+    <?php if(!empty($model)):?>
+    <h3><?= Html::encode("Productos") ?></h3>
+        <table class="table table-stripped">
+        <?php foreach($model as $producto) :?>
+            <tr>
+                <td>
+                    <?= $producto->codigo; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    <?php endif;?>
+
+</div>
+
